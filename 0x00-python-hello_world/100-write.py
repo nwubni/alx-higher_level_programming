e5bf2049ff29a114168d215b27caa62582b344fe
Nakuru Wubni <nwubni@gmail.com>
2023-06-05 19:03:55 +0100

    Hello, write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python3
 import sys
 
-message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-sys.stderr.write(message + "\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
