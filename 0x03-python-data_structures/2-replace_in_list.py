e257655aba5f32c1128dc165a813c6cebe0077ae
Nakuru Wubni <nwubni@gmail.com>
2023-06-10 10:39:48 +0100

    Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@
 def replace_in_list(my_list, idx, element):
     item_count = len(my_list)
 
-    if idx >= 0 or idx < item_count:
+    if idx >= 0 and idx < item_count:
         my_list[idx] = element
 
     return my_list
