20bd1785f4ec648e56c6a721a7452c6b663f946b
Nakuru Wubni <nwubni@gmail.com>
2023-06-14 14:50:08 +0100

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    numerals = {'M': 1000, 'D': 500, 'C': 100,
+                'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+    ans = 0
+    prev = 2000
+
+    for c in roman_string:
+        if prev < numerals[c]:
+            ans -= prev * 2
+
+        ans += numerals[c]
+        prev = numerals[c]
+
+    return ans
