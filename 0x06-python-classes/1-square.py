1141cc05db5e47286372297f15e6e46fb319c455
Nakuru Wubni <nwubni@gmail.com>
2023-06-27 13:14:33 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class of square"""
+
+
+class Square:
+    """A class of square"""
+
+    def __init__(self, size) -> None:
+        """Initializes an instance of Square class
+            Args:
+                size (int): Size of the square
+        """
+        self.__size = size
