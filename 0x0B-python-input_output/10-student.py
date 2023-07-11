99ec86d99e03da7cac79b6e9215183aec545cf18
Nakuru Wubni <nwubni@gmail.com>
2023-07-11 17:35:04 +0100

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,11 +7,13 @@ class Student:
     """Student class"""
 
     def __init__(self, first_name, last_name, age):
+        """Class initializer"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """Returns dictionary description of object"""
         if attrs is None:
             return self.__dict__
 
