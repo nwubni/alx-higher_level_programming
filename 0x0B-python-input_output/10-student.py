27715a4980660aac440f32b87b41f9b4fab34e97
Nakuru Wubni <nwubni@gmail.com>
2023-07-11 17:28:16 +0100

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
