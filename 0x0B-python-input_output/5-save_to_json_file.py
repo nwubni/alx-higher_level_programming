881ef3fd32f5d0447bfff30cb75066d6315489f9
Nakuru Wubni <nwubni@gmail.com>
2023-07-11 15:36:47 +0100

    Save Object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,5 +12,5 @@ def save_to_json_file(my_obj, filename):
             filename: File name to use
     """
 
-    with open(filename, "w") as f:
-        json.dumps(my_obj, f)
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
