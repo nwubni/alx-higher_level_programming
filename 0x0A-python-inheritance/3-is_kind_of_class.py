34237291f48fe5a6fb1b2d5dddf9b9df119965d7
Nakuru Wubni <nwubni@gmail.com>
2023-07-12 13:45:21 +0100

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Compares instances of two variables"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Compares instances of two variables
+        Args:
+            obj: Param 1
+            a_class: Param 2
+    """
+    return isinstance(obj, a_class)
