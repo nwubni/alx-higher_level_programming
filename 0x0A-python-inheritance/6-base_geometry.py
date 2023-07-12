da9e8bd6669741bf64edde835b6e0a4a39dafe32
Nakuru Wubni <nwubni@gmail.com>
2023-07-12 16:16:35 +0100

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Geometry Module"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class"""
+    pass
+
+    def area(self):
+        """Computes Shape's Area"""
+        raise Exception("area() is not implemented")
