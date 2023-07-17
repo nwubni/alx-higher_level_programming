921c4d96526a250aa9e0e22ffd20df19921e056a
Nakuru Wubni <nwubni@gmail.com>
2023-07-17 15:53:10 +0100

    Almost a circle

---
+++ 0x0C-python-almost_a_circle/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+try:
+    #     r = Rectangle(10, 2)
+    #     r.width = -10
+    # except Exception as e:
+    #     print("[{}] {}".format(e.__class__.__name__, e))
+
+    # try:
+    #     r = Rectangle(10, 2)
+    #     r.x = {}
+    # except Exception as e:
+    #     print("[{}] {}".format(e.__class__.__name__, e))
+
+    # try:
+    #     Rectangle(10, 2, 3, -1)
+    # except Exception as e:
+    #     print("[{}] {}".format(e.__class__.__name__, e))
