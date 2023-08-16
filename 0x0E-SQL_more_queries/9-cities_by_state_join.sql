18508d0b1665e79a945832db3b342b81462d98be
Nakuru Wubni <nwubni@gmail.com>
2023-08-16 22:15:16 +0100

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- Cities by States
-SELECT id, name, (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
-ORDER BY id ASC;
+INNER JOIN states
+ON cities.state_id = states.id;
