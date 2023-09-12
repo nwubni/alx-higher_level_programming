b5fe01a85c632c41fa5c7e8c774c480dc039af6c
Nakuru Wubni <nwubni@gmail.com>
2023-09-12 13:07:45 +0100

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const parsedNumber = parseInt(process.argv[2]);
+
+if (!isNaN(parsedNumber)) {
+    console.log(`My number: ${parsedNumber}`);
+} else {
+    console.log("Not a number");
+}
