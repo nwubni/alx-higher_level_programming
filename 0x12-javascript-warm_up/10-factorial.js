1b2d2c7b56353026d227b3726c16e23e94e060c6
Nakuru Wubni <nwubni@gmail.com>
2023-09-13 11:19:35 +0100

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial(x) {
+    if (x == undefined || x === null || x <= 1)
+        return 1;
+
+    return (x * factorial(x - 1));
+}
+
+const ans = factorial(process.argv[2]);
+console.log(ans);
