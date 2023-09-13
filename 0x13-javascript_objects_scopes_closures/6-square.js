4b70f65def5c620673fde6223678ce08f8bde646
Nakuru Wubni <nwubni@gmail.com>
2023-09-13 15:27:58 +0100

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    let output = '';
+
+    if (c === undefined) { c = 'X'; }
+
+    for (let i = 1; i <= this.height; i++) {
+      for (let j = 1; j <= this.width; j++) { output += c; }
+
+      console.log(output);
+      output = '';
+    }
+  }
+};
