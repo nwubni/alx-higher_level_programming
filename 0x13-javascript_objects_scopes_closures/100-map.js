9ae21a7dc57bc3c3d20cbc1809eac87286497c90
Nakuru Wubni <nwubni@gmail.com>
2023-09-13 16:24:50 +0100

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data');
+const list = require('./100-data.js').list;
 const res = list.map((x, index) => x * index);
 
-console.log(JSON.stringify(list));
-console.log(JSON.stringify(res));
+console.log(list);
+console.log(res);
