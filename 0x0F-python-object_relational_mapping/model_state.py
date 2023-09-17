cc32b838c6b0498b8ad574d74648e14813416a70
Nakuru Wubni <nwubni@gmail.com>
2023-09-17 09:35:07 +0100

    First state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Create a database table from a SQLAlchemy model.
+"""
+
+import sys
+from model_state import Base
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    connection_string = f'mysql+mysqldb://{username}:{password}@localhost/{database}'
+
+    engine = create_engine(connection_string, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
