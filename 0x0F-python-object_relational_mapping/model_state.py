e05cb5fa2f7a4b208f29cb03ea347c6c8b57eca4
Nakuru Wubni <nwubni@gmail.com>
2023-09-17 12:48:33 +0100

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,8 +4,8 @@ Create a database table from a SQLAlchemy model.
 """
 
 import sys
-from model_state import Base
-from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
