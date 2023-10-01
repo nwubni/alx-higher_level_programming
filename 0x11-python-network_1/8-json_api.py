0f7858c8a05e2cbb9210638f95eac343a1b6b202
Nakuru Wubni <nwubni@gmail.com>
2023-10-01 20:36:08 +0100

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     alpha = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": alpha}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    result = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
-        if response == {}:
+        res = result.json()
+        if res == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("[{}] {}".format(res.get("id"), res.get("name")))
     except ValueError:
         print("Not a valid JSON")
