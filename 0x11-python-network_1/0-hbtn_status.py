b200d0754c682d993a6db3a7a0aa810b572650f8
Nakuru Wubni <nwubni@gmail.com>
2023-10-01 20:24:24 +0100

    Search API

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that
+What's my status? #0
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as result:
+        content = result.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
