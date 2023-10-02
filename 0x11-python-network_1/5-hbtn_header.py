8a559c39d484e6e9c200f79c3aec83c15d9ba643
Nakuru Wubni <nwubni@gmail.com>
2023-10-02 14:37:55 +0100

    Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Response header value #1
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    print(req.headers.get("X-Request-Id"))
